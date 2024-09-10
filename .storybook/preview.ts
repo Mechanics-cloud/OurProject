15f13790aaf930c4f1ef9958e8ab521d9fbe7000
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-10 16:15:25 +0300

    INCUBATOR-2 init: add dark-mode to Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,21 @@
 import type { Preview } from '@storybook/react'
 import '../src/styles/globals.css'
+import { themes } from '@storybook/theming'
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
