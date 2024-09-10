968d6b4aebb747330ecba450a30fce86c93effb5
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-10 15:49:14 +0300

    INCUBATOR-2 feat: add Card component

---
+++ src/common/components/card/Card.tsx
@@ -0,0 +1,11 @@
+import { ComponentProps } from 'react'
+
+type Props = ComponentProps<'div'>
+
+export const Card = ({className, children, ...props}: Props) => {
+  return (
+    <div className={'bg-dark-500 rounded-sm border border-dark-300 px-6 py-4 text-light-100'}
+         {...props}>
+      {children}</div>
+  )
+}
