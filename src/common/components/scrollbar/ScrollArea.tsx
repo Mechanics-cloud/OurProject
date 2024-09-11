af16c6db7574ec2a931cdc413626c0020472de84
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-11 12:32:07 +0300

    IN-14 feat: add stories for ScrollArea component

--- src/common/components/scrollbar/ScrollArea.tsx
+++ src/common/components/scrollbar/ScrollArea.tsx
@@ -17,7 +17,7 @@ export const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar/>
+    <ScrollBar orientation={orientation}/>
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ))
--- src/common/components/scrollbar/Scrollbar.stories.tsx
+++ src/common/components/scrollbar/Scrollbar.stories.tsx
@@ -1,7 +1,5 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react'
 import { ScrollArea } from '@/common/components/scrollbar/ScrollArea'
-import { Scrollbar } from '@radix-ui/react-scroll-area'
-import { ScrollBar } from '@/common/components/scrollbar/Scrollbar'
 
 const meta = {
   title: 'Component/Scrollbar',
@@ -10,32 +8,34 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+} satisfies Meta<typeof ScrollArea>
 
-} satisfies Meta<typeof ScrollArea>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 export const BasicVerticalScrollbar: Story = {
   args: {
-    className: "h-[200px] w-[350px] rounded-md border p-3",
-    children: 'Jokester began sneaking into the castle in the middle of the night and leaving\n' +
-      '  jokes all over the place: under the king\'s pillow, in his soup, even in the\n' +
-      '  royal toilet. The king was furious, but he couldn\'t seem to stop Jokester. And\n' +
-      '  then, one day, the people of the kingdom discovered that the jokes left by\n' +
-      '  Jokester were so funny that they couldn\'t help but laugh. And once they\n' +
-      '  started laughing, they couldn\'t stop.',
+    className: 'h-[200px] w-[350px] rounded-md border p-3',
+    children:
+      'Закон исключённого третьего, конечно, подчеркивает примитивный гений. Концепция очевидна не для всех. Сомнение поразительно. Созерцание транспонирует мир. Атомистика раскладывает на элементы естественный бабувизм. Культ джайнизма включает в себя поклонение Махавире и другим тиртханкарам, поэтому бабувизм реально принимает во внимание язык образов. Закон внешнего мира нетривиален. Культ джайнизма включает в себя поклонение Махавире и другим тиртханкарам, поэтому исчисление предикатов осмысленно понимает под собой субъективный конфликт, при этом буквы А, В, I, О символизируют соответственно общеутвердительное, общеотрицательное, частноутвердительное и частноотрицательное суждения. Дедуктивный метод амбивалентно транспонирует напряженный закон исключённого третьего. Диалектика, конечно, иллюзорна.bБабувизм принимает во внимание дедуктивный метод. Освобождение, конечно, методологически подчеркивает даосизм. Вещь в себе, как принято считать, естественно рассматривается трансцендентальный здравый смысл, однако Зигварт считал критерием истинности необходимость и общезначимость, для которых нет никакой опоры в объективном мире.',
   },
-};
+}
 
 export const BasicHorizontalScrollbar: Story = {
   args: {
-    className: "h-[200px] w-[350px] rounded-md border p-3",
-    children: <div className={'flex gap-2'}>
-      {Array.from({length: 3}).fill(0).map((el, index) => (
-        <div key={index} className={'w-[150px] h-28 bg-cyan-700'}></div>
-      ))}
-      <ScrollBar orientation={'horizontal'}/>
-    </div>,
+    orientation: 'horizontal',
+    className: 'h-[200px] w-[350px] rounded-md border p-3',
+    children: (
+      <div className={'flex gap-2'}>
+        {Array.from({ length: 3 })
+          .fill(0)
+          .map((el, index) => (
+            <div
+              key={index}
+              className={'w-[150px] h-28 bg-accent-700'}
+            ></div>
+          ))}
+      </div>
+    ),
   },
-};
+}
