16989482c560b674a800a766ed748a7100bdd842
Sun-ny11 <spetrow79@gmail.com>
2024-09-12 17:46:17 +0300

    feat: datePicker is done

--- src/common/components/calendar/Calendar.tsx
+++ src/common/components/calendar/Calendar.tsx
@@ -1,8 +1,9 @@
 import { DayPicker, DayPickerProps } from 'react-day-picker'
 
+import { ru } from 'date-fns/locale'
 type Props = {} & DayPickerProps
 
-export const DatePicker = ({ ...restProps }: Props) => {
+export const Calendar = ({ ...restProps }: Props) => {
   const interactiveStyles =
     'focus-visible:border-2 focus-visible:border-accent-300 focus-visible:outline-none hover:bg-accent-700 hover:transition-colors hover:duration-200'
   const buttonBaseStyles = `w-9 h-9 bg-light-900 rounded-full flex items-center justify-center ${interactiveStyles}`
@@ -10,55 +11,54 @@ export const DatePicker = ({ ...restProps }: Props) => {
   return (
     <DayPicker
       {...restProps}
-      weekStartsOn={1}
       classNames={{
-        root: 'bg-dark-500 text-white w-[300px] max-h-[350px] px-6 py-4',
-        months: 'relative',
-
-        nav: ' absolute right-0 flex ',
-        button_previous: buttonBaseStyles,
         button_next: buttonBaseStyles,
+        button_previous: buttonBaseStyles,
+
+        caption_label: 'text-lg  font-bold',
         chevron: 'w-4 h-4 fill-light-100 ',
-
-        month: '',
-        month_caption: 'mb-3 h-9 flex items-center',
-        caption_label: 'text-lg  ',
-
-        month_grid: 'border-collapse', //table
-
-        weekdays: '', //tr inside thead
-        weekday: 'text-light-900 w-9 h-9 ', //th
-
-        weeks: 'before:block before:h-3', //tbody
-        week: '', //tr inside tbody
         day: 'text-center w-9 h-9 [&:nth-last-child(-n+2)]:text-danger-300 rounded-full ', //td
         day_button: ` w-full h-full rounded-full  ${interactiveStyles} `,
 
-        today: 'text-accent-500 ',
-
         disabled: '1',
-
         dropdown: 'block w-2 h-2',
         dropdown_root: 'relative',
+
         dropdowns: '3',
 
         focused: '',
-
         footer: '',
 
         hidden: '6',
-        outside: '!text-light-900',
+        month: '',
+        month_caption: 'mb-3 h-9 flex items-center ',
+        month_grid: 'border-collapse', //table
+
+        months: 'relative',
 
         months_dropdown: '',
 
+        nav: ' absolute right-0 flex ',
+        outside: '!text-light-900',
         range_end: 'bg-accent-900  rounded-r-full rounded-none',
+
         range_middle: 'bg-accent-900 rounded-none ',
+
         range_start: 'bg-accent-900  rounded-l-full rounded-none',
 
+        root: 'bg-dark-500 text-white w-[300px]  px-6 py-4 ',
         selected: 'bg-accent-900  ',
 
+        today: 'text-accent-500 !font-bold',
+
+        week: '', //tr inside tbody
         week_number: '',
         week_number_header: '',
+
+        weekday: 'text-light-900 w-9 h-9 font-medium', //th
+
+        weekdays: '', //tr inside thead
+        weeks: 'before:block before:h-3', //tbody
         years_dropdown: '',
       }}
     />
--- src/common/components/popover/Popover.tsx
+++ src/common/components/popover/Popover.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as React from 'react'
+
 import * as PopoverPrimitive from '@radix-ui/react-popover'
 
 const Popover = PopoverPrimitive.Root
@@ -12,17 +13,18 @@ const PopoverAnchor = PopoverPrimitive.Anchor
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
+>(({ align = 'center', className, sideOffset = 4, ...props }, ref) => (
   <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
-      ref={ref}
       align={align}
-      sideOffset={sideOffset}
       className={className}
+      ref={ref}
+      sideOffset={sideOffset}
       {...props}
     />
   </PopoverPrimitive.Portal>
 ))
+
 PopoverContent.displayName = PopoverPrimitive.Content.displayName
 
-export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
+export { Popover, PopoverAnchor, PopoverContent, PopoverTrigger }
