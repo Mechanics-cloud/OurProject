49dffd581ea45f8daebe92a9fdac25465b706fbb
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-12 16:51:33 +0300

    IN-8 feat: finish styles for different states

--- src/common/components/tabs/Tabs.tsx
+++ src/common/components/tabs/Tabs.tsx
@@ -33,10 +33,12 @@ const Tabs = React.forwardRef<
               'group',
               'border-dark-100 border-b-2 text-dark-100',
               'flex-1 px-4 py-1.5',
+              'disabled:radix-state-active:text-accent-900 disabled:radix-state-active:border-b-accent-900',
+              'disabled:radix-state-inactive:text-dark-300 disabled:radix-state-inactive:border-b-dark-300',
               'radix-state-active:border-b-accent-500 radix-state-active:bg-gray-900 radix-state-active:text-accent-500 focus-visible:radix-state-active:border-b-transparent radix-state-inactive:bg-dark-700',
               'focus:radix-state-active:border-active-500',
-              'focus:z-10 focus:outline-none focus-visible:ring focus-visible:ring-accent-300',
-              'hover:radix-state-active:bg-accent-900/[.15] hover:radix-state-inactive:bg-accent-900/[.15]',
+              'focus:z-10 focus:outline-none focus-visible:ring focus-visible:rounded focus-visible:ring-accent-300',
+              'enabled:hover:radix-state-active:bg-accent-900/[.15] enabled:hover:radix-state-inactive:bg-accent-900/[.15]',
               'active:bg-accent-100/[.15]'
             )}
             disabled={disabled ?? false}
