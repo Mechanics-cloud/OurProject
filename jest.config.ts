6203cce44a3f2130738524aa56a457bc6f065936
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-13 22:21:29 +0300

    IN-12 fix: apply prettier for the whole project

--- jest.config.ts
+++ jest.config.ts
@@ -10,16 +10,12 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   moduleNameMapper: {
-    // '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // testEnvironment: 'jsdom',
   testEnvironment: 'jest-environment-jsdom',
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   transform: {
-    // '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
 }
