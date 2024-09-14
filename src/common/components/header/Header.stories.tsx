9dc4332ec80a4738ee63c018342e0b98b4960133
KvashninAlexander2023 <study_python@mail.ru>
2024-09-14 22:41:25 +0500

    IN-9  chore: save before update

--- src/common/components/header/Header.stories.tsx
+++ src/common/components/header/Header.stories.tsx
@@ -1,9 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Button } from '@/common/components/button'
-import { Typography } from '@/common/components/typography'
-import Image from 'next/image'
-
 import { Header } from './Header'
 
 const meta = {
@@ -11,11 +7,14 @@ const meta = {
   parameters: {
     layout: 'centered',
   },
-  tags: ['autodocs'],
   title: 'Component/Header',
 } satisfies Meta<typeof Header>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const noRegHeader: Story = {}
+export const HeaderIsAuthFalse: Story = {
+  args: {
+    isAuth: false,
+  }
+}
--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -1,30 +1,33 @@
 import * as React from 'react'
 
-import { typographyVariants } from '@/common/components/typography'
-import { clsx } from 'clsx'
-import Image from 'next/image'
+import { Typography } from '@/common/components/typography'
+import OutlineBell from '../../../assets/icons/OutlineBell'
+import { Button } from '../button'
 
-const Header = () => {
+type HeaderProps = {
+  isAuth: boolean
+}
+
+const Header = (props: HeaderProps) => {
+  const { isAuth } = props
   return (
     <header
       className={
-        'fixed top-0 left-0 w-full h-16 bg-gray-800 text-white flex items-center justify-between px-4'
+        'fixed top-0 left-0 w-full h-[60px] bg-gray-800 text-white flex items-center justify-between pr-16 border-b border-b-dark-300'
       }
     >
-      <p className={'text-lg font-bold'}>Inctagram</p>
+      <Typography variant={'large'} className={'ml-14 py-3'}>Inctagram</Typography>
       <div className={'flex items-center space-x-4'}>
-        <select className={'bg-gray-700 text-white rounded px-2 py-1'}>
-          <option value={'ru'}>Русский</option>
+        {isAuth && <OutlineBell
+          className="cursor-pointer w-6 h-6 mr-12"
+          onClick={() => alert('Картинка нажата!')}
+        />}
+        <select className={'bg-gray-700 text-white rounded px-2 py-3 w-[163px] h-9 mr-9'}>
           <option value={'en'}>English</option>
+          <option value={'ru'}>Русский</option>
         </select>
-        <Image
-          alt={'Logo'}
-          className={'cursor-pointer'}
-          height={30}
-          onClick={() => alert('Картинка нажата!')} // Пример действия при клике
-          src={'/path/to/your/image.png'} // Укажите путь к вашей картинке
-          width={30}
-        />
+        {!isAuth && <div><Button variant={'text'} className={'mr-6'}>Log in</Button><Button>Sing up</Button></div>}
+
       </div>
     </header>
   )
@@ -32,10 +35,3 @@ const Header = () => {
 
 export { Header }
 
-{
-  /* <div className='w-screen bg-red-300 h-20'>
-<div className='h-[60px] w-full bg-slate-400/50'>
-  hello
-</div>
-</div> */
-}
