4bb753d2e2cdd426f5008939bfc37cca79d00d49
KvashninAlexander2023 <study_python@mail.ru>
2024-09-15 23:57:05 +0500

    IN-29 commit after lint:fix

--- src/common/components/IconStories/Icons.stories.tsx
+++ src/common/components/IconStories/Icons.stories.tsx
@@ -1,4 +1,5 @@
 import type { StoryObj } from '@storybook/react'
+
 // import * as filledIcons from '../../../assets/icons/outlineIcons'
 import * as filledIcons from '../../../assets/icons/filledIcons'
 import * as outlineIcons from '../../../assets/icons/outlineIcons'
@@ -15,20 +16,26 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const FilledIcons = () => (
-  <div className="flex flex-wrap mx-12">
+  <div className={'flex flex-wrap mx-12'}>
     {Object.entries(filledIcons).map(([name, Icon]) => (
-      <div key={name} className="inline-flex items-center flex-none w-1/4 mr-5 mb-5">
-        <Icon className='w-6 h-6 mr-3' /> {name}
+      <div
+        className={'inline-flex items-center flex-none w-1/4 mr-5 mb-5'}
+        key={name}
+      >
+        <Icon className={'w-6 h-6 mr-3'} /> {name}
       </div>
     ))}
   </div>
-);
+)
 export const OutlineIcons = () => (
-  <div className="flex flex-wrap mx-12">
+  <div className={'flex flex-wrap mx-12'}>
     {Object.entries(outlineIcons).map(([name, Icon]) => (
-      <div key={name} className="inline-flex items-center flex-none w-1/4 mr-5 mb-5">
-        <Icon className="w-6 h-6 mr-3"/> {name}
+      <div
+        className={'inline-flex items-center flex-none w-1/4 mr-5 mb-5'}
+        key={name}
+      >
+        <Icon className={'w-6 h-6 mr-3'} /> {name}
       </div>
     ))}
   </div>
-);
+)
