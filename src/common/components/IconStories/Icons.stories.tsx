f519a4cf9ac311fc68fe701f0975c0e0f78134d6
KvashninAlexander2023 <study_python@mail.ru>
2024-09-15 22:23:52 +0500

    IN-29 feet: aad Icon Stories

---
+++ src/common/components/IconStories/Icons.stories.tsx
@@ -0,0 +1,34 @@
+import type { StoryObj } from '@storybook/react'
+import * as filledIcons from '../../../assets/icons'
+// import * as filledIcons from '../../../assets/icons/filledIcons'
+// import * as outlineIcons from '../../../assets/icons/outlineIcons'
+
+const meta = {
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  title: 'Disign Sistem/Icons',
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const FilledIcons = () => (
+  <div className="flex flex-wrap mx-12">
+    {Object.entries(filledIcons).map(([name, Icon]) => (
+      <div key={name} className="inline-flex items-center flex-none w-1/4 mr-5 mb-5">
+        <Icon className='w-6 h-6 mr-3' /> {name}
+      </div>
+    ))}
+  </div>
+);
+// export const OutlineIcons = () => (
+//   <div className="flex flex-wrap mx-12">
+//     {Object.entries(outlineIcons).map(([name, Icon]) => (
+//       <div key={name} className="inline-flex items-center flex-none w-1/5 mr-5 mb-5">
+//         <Icon className="w-8 h-8 mr-2.5"/> {name}
+//       </div>
+//     ))}
+//   </div>
+// );
