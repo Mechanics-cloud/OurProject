35e448f90b5309d59c1e5c3033745c570fb18050
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-19 16:56:41 +0300

    init: test lint-staged

---
+++ src/common/utils/__test__/check.tsx
@@ -0,0 +1,11 @@
+// @flow
+import * as React from 'react'
+
+type Props = {}
+export const Test = (props: Props) => {
+  return (
+    <div>
+      <button type={'button'}></button>
+    </div>
+  )
+}
