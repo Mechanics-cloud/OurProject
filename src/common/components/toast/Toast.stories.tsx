f0398f7df14621f520f4153ec62c1d15d6f910e9
VLAD <buladzislau@gmail.com>
2024-09-20 20:42:53 +0300

    IN-17 feat: add Toast (Alert) behaviour

---
+++ src/common/components/toast/Toast.stories.tsx
@@ -0,0 +1,52 @@
+import { toast } from 'react-toastify'
+
+import { toastPositions } from '@/common/components/toast/helpers'
+import { Meta, StoryObj } from '@storybook/react'
+
+import { ToastContainer } from './ToastContainer'
+
+const meta: Meta<typeof ToastContainer> = {
+  argTypes: {
+    position: {
+      control: { type: 'select' },
+      options: toastPositions,
+    },
+  },
+  component: ToastContainer,
+  parameters: {
+    layout: 'centered',
+  },
+  render: (args) => {
+    const notifyError = () => toast.error('Error message')
+    const notifySuccess = () => toast.success('Success message')
+
+    return (
+      <div className={'flex flex-col gap-10'}>
+        <button type="button" onClick={notifyError}>Error!</button>
+        <button type="button" onClick={notifySuccess}>Success!</button>
+        <ToastContainer {...args} />
+      </div>
+    )
+  },
+  tags: ['autodocs'],
+  title: 'Component/Toast',
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    autoClose: 3000,
+    closeOnClick: true,
+    hideProgressBar: true,
+    icon: false,
+    position: 'bottom-left',
+  },
+}
+
+export const WithProgressBar: Story = {
+  args: {
+    hideProgressBar: false,
+  },
+}
