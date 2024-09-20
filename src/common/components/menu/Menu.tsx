7b3f7bfaf7486503af3ef2d525afeee14aa54905
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-20 20:34:24 +0300

    IN-82 refactor: change mobile menu component

--- src/common/components/menu/Menu.tsx
+++ src/common/components/menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import {
   Home,
@@ -14,83 +14,81 @@ import {
   PlusSquareOutline,
   SearchOutline,
 } from '@/assets/icons/outlineIcons'
-import { MenuItem } from '@/common/components/menu/MenuItem'
+import { matchesPathname } from '@/common/components/menu/matchesPathname'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 const PATHS = {
   HOME: '/',
-  MESSENGER: '/messenger',
-  PROFILE: '/profile',
-  PUBLICATION: '/publication',
-  SEARCH: '/search',
+  MESSENGER: '/messenger/*',
+  PROFILE: '/profile/*',
+  PUBLICATION: '/publication/*',
+  SEARCH: '/search/*',
 } as const
 
 export type PathsType = (typeof PATHS)[keyof typeof PATHS]
 
-const linksForMenu = [
-  {
-    iconFilled: MessageCircle,
-    iconOutlined: MessageCircleOutline,
-    path: PATHS.MESSENGER,
-  },
-  {
-    iconFilled: Search,
-    iconOutlined: SearchOutline,
-    path: PATHS.SEARCH,
-  },
-  {
-    iconFilled: Person,
-    iconOutlined: PersonOutline,
-    path: PATHS.PROFILE,
-  },
-]
-
 export const Menu = () => {
   const router = useRouter()
-  const path = router.pathname
+  const href = router.pathname
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
     <nav className={'min-w-[360px] w-full bg-dark-00 border-t border-dark-300'}>
       <ul className={'flex w-full justify-evenly py-[18px]'}>
-        <li key={'home'}>
+        <li>
           <Link href={PATHS.HOME}>
-            {path === PATHS.HOME ? (
-              <Home className={'w-6 h-6 text-accent-500'} />
+            {matchesPathname(href, PATHS.HOME) ? (
+              <Home className={'size-6 text-accent-500'} />
             ) : (
-              <HomeOutline className={'w-6 h-6'} />
+              <HomeOutline className={'size-6'} />
             )}
           </Link>
         </li>
-        <li
-          className={'group'}
-          key={'publication'}
-        >
+        <li className={'group'}>
           <button
             onClick={() => {
-              alert('Hey!')
+              setIsModalOpen(true)
             }}
             type={'button'}
           >
             <PlusSquareOutline
-              className={'w-6 h-6 group-hover:hidden group-active:hidden'}
+              className={'size-6 group-hover:hidden group-active:hidden'}
             />
             <PlusSquare
               className={
-                'w-6 h-6 hidden group-hover:block group-active:block text-accent-500'
+                'size-6 hidden group-hover:block group-active:block text-accent-500'
               }
             />
           </button>
         </li>
-        {linksForMenu.map((link, index) => (
-          <MenuItem
-            currentPath={path}
-            iconFilled={link.iconFilled}
-            iconOutlined={link.iconOutlined}
-            key={index}
-            path={link.path}
-          />
-        ))}
+        <li>
+          <Link href={PATHS.MESSENGER}>
+            {matchesPathname(href, PATHS.MESSENGER) ? (
+              <MessageCircle className={'size-6 text-accent-500'} />
+            ) : (
+              <MessageCircleOutline className={'size-6'} />
+            )}
+          </Link>
+        </li>
+        <li>
+          <Link href={PATHS.SEARCH}>
+            {matchesPathname(href, PATHS.SEARCH) ? (
+              <Search className={'size-6 text-accent-500'} />
+            ) : (
+              <SearchOutline className={'size-6'} />
+            )}
+          </Link>
+        </li>
+        <li>
+          <Link href={PATHS.PROFILE}>
+            {matchesPathname(href, PATHS.PROFILE) ? (
+              <Person className={'size-6 text-accent-500'} />
+            ) : (
+              <PersonOutline className={'size-6'} />
+            )}
+          </Link>
+        </li>
       </ul>
     </nav>
   )
