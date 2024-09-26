b0f61633fa6e211c16492c554eaeb34c797dc55c
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-26 11:33:21 +0300

    IN-38 refactor: fix styles, fix imports

--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -2,11 +2,14 @@ import {
   GithubSvgrepoCom31,
   GoogleSvgrepoCom1,
 } from '@/assets/icons/filledIcons'
-import { Typography, typographyVariants } from '@/common/components'
-import { Button } from '@/common/components/button'
-import { Card } from '@/common/components/card'
-import { FormCheckbox } from '@/common/components/form/FormCheckbox'
-import { FormTextField } from '@/common/components/form/FormTextField'
+import {
+  Button,
+  Card,
+  FormCheckbox,
+  FormTextField,
+  Typography,
+  typographyVariants,
+} from '@/common'
 import { cn } from '@/common/utils/cn'
 import { useSignUp } from '@/pages/auth/sign-up/_useSignUp'
 import Link from 'next/link'
@@ -45,18 +48,16 @@ const SignUp = () => {
         </div>
 
         <form
-          className={'min-w-80'}
+          className={'min-w-80 flex flex-col gap-6'}
           onSubmit={onSubmit}
         >
           <FormTextField
-            className={'mb-6'}
             control={control}
             label={'Username'}
             name={'username'}
             placeholder={'Enter your username'}
           />
           <FormTextField
-            className={'mb-6'}
             control={control}
             error={errors.email?.message}
             label={'Email'}
@@ -64,7 +65,6 @@ const SignUp = () => {
             placeholder={'Enter your email'}
           />
           <FormTextField
-            className={'mb-6'}
             control={control}
             error={errors.password?.message}
             label={'Password'}
@@ -73,7 +73,6 @@ const SignUp = () => {
             type={'password'}
           />
           <FormTextField
-            className={'mb-6'}
             control={control}
             error={errors.confirm?.message}
             label={'Password confirmation'}
@@ -81,40 +80,42 @@ const SignUp = () => {
             placeholder={'Confirm your password'}
             type={'password'}
           />
-          <div
-            className={cn(
-              typographyVariants({ variant: 'small' }),
-              `flex items-center gap-1 mb-3`
-            )}
-          >
-            <FormCheckbox
-              control={control}
-              id={'agreement'}
-              label={`I agree to the`}
-              name={'agreesToTOS'}
-              typographyVariant={'small'}
-            />
-            <Typography
-              href={''}
-              variant={'smallLink'}
+          <div>
+            <div
+              className={cn(
+                typographyVariants({ variant: 'small' }),
+                `flex items-center gap-1 mb-3`
+              )}
             >
-              Terms of Service
-            </Typography>
-            and
-            <Typography
-              href={''}
-              variant={'smallLink'}
+              <FormCheckbox
+                control={control}
+                id={'agreement'}
+                label={`I agree to the`}
+                name={'agreesToTOS'}
+                typographyVariant={'small'}
+              />
+              <Typography
+                href={''}
+                variant={'smallLink'}
+              >
+                Terms of Service
+              </Typography>
+              and
+              <Typography
+                href={''}
+                variant={'smallLink'}
+              >
+                Privacy Policy
+              </Typography>
+            </div>
+            <Button
+              className={'w-full mb-[18px]'}
+              disabled={!isValid}
+              type={'submit'}
             >
-              Privacy Policy
-            </Typography>
+              Sign Up
+            </Button>
           </div>
-          <Button
-            className={'w-full mb-[18px]'}
-            disabled={!isValid}
-            type={'submit'}
-          >
-            Sign Up
-          </Button>
         </form>
 
         <div className={'flex flex-col gap-y-1.5 items-center'}>
