ed3afe536dedfca9bfd65648477f06a1eb553d61
Daria Metelitsa <dariametelitsa@gmail.com>
2024-09-28 13:44:38 +0300

    IN-131 feat: add Content-Type

--- src/features/signUp/api/instances.ts
+++ src/features/signUp/api/instances.ts
@@ -2,5 +2,8 @@ import axios from 'axios'
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_INCTAGRAM_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,
 })
