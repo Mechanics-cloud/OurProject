84cadd9839a4dfbf9b17548f425e9086f9490663
OchirovBair <pactuc@gmail.com>
2024-09-30 21:30:49 +0800

    IN-141 refactor: add internationalization to app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
-import { Loader, Menu } from '@/common'
-import SignUp from '@/pages/auth/sign-up'
+import { Button } from '@/common/components/button'
+import { typographyVariants } from '@/common/components/typography'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={'flex flex-col justify-center items-center gap-5 h-full'}>
-      <Menu />
-      <SignUp />
+      <Button
+        asChild
+        className={typographyVariants({ variant: 'h3' })}
+      >
+        <Link href={'/profile'}>profile</Link>
+      </Button>
+      <Button
+        asChild
+        className={typographyVariants({ variant: 'h3' })}
+      >
+        <Link href={'/publication'}>publication</Link>
+      </Button>
     </div>
   )
 }
