d83c1bacf3a4f4f24be434f5d2de9af11025891c
tanya-kostecki <tanya.pavlova1294@gmail.com>
2024-10-01 12:25:09 +0200

    IN-100 refactor: map placeholder images

--- src/pages/profile/[[...id]].tsx
+++ src/pages/profile/[[...id]].tsx
@@ -8,6 +8,17 @@ import image2 from '../../assets/images/image2.jpg'
 import image3 from '../../assets/images/image3.jpg'
 import image4 from '../../assets/images/image4.jpg'
 
+const placeholderImages = [
+  { id: 1, img: image1 },
+  { id: 2, img: image2 },
+  { id: 3, img: image3 },
+  { id: 4, img: image4 },
+  { id: 5, img: image1 },
+  { id: 6, img: image2 },
+  { id: 7, img: image3 },
+  { id: 8, img: image4 },
+]
+
 function Profile() {
   return (
     <div className={'flex ml-[220px]'}>
@@ -62,54 +73,15 @@ function Profile() {
           </div>
         </div>
         <div className={'grid grid-cols-4 grid-rows-auto gap-x-3 gap-y-3'}>
-          <Image
-            alt={'1'}
-            height={228}
-            src={image1}
-            width={342}
-          />
-          <Image
-            alt={'2'}
-            height={228}
-            src={image2}
-            width={342}
-          />
-          <Image
-            alt={'3'}
-            height={228}
-            src={image3}
-            width={342}
-          />
-          <Image
-            alt={'4'}
-            height={228}
-            src={image4}
-            width={342}
-          />
-          <Image
-            alt={'1'}
-            height={228}
-            src={image1}
-            width={342}
-          />
-          <Image
-            alt={'2'}
-            height={228}
-            src={image2}
-            width={342}
-          />
-          <Image
-            alt={'3'}
-            height={228}
-            src={image3}
-            width={342}
-          />
-          <Image
-            alt={'4'}
-            height={228}
-            src={image4}
-            width={342}
-          />
+          {placeholderImages.map((image) => (
+            <Image
+              alt={'image'}
+              height={228}
+              key={image.id}
+              src={image.img}
+              width={342}
+            />
+          ))}
         </div>
       </div>
     </div>
