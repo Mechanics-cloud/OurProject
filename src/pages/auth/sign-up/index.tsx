a72cc580d2fdc4962a66bcc7ec2c1bad96695589
tanya-kostecki <tanya.pavlova1294@gmail.com>
2024-10-14 16:14:53 +0200

    IN-155 refactor: after merge

--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -1,6 +1,5 @@
-import { Loader, Paths, getBaseLayout } from '@/common'
+import { Paths, getBaseLayout } from '@/common'
 import { withRedirectForAuthorize } from '@/common/HOC/withRedirectForAuthorize'
-import { generalStore } from '@/core/store'
 import { SignUpForm } from '@/features/auth'
 import authStore from '@/features/auth/model/authStore'
 import Router from 'next/router'
@@ -9,18 +8,8 @@ const SignUp = () => {
   if (authStore.profile) {
     Router.push(Paths.profile)
   }
-  const { isLoading } = generalStore
 
-  return (
-    <div
-      className={
-        'md:mt-[24px] mt-4 md:w-[378px] mx-auto box-border border-transparent'
-      }
-    >
-      {isLoading && <Loader />}
-      <SignUpForm />
-    </div>
-  )
+  return <SignUpForm />
 }
 
 SignUp.getLayout = getBaseLayout
