cc3e91a1ea6f2e3b30414b677b1b55dbabad8440
Daria Metelitsa <dariametelitsa@gmail.com>
2024-11-01 18:49:31 +0300

    IN-150 feat: add skeleton component

---
+++ src/common/components/skeleton/Skeleton.tsx
@@ -0,0 +1,15 @@
+import { HTMLAttributes } from 'react'
+
+import { cn } from '@/common'
+
+export const Skeleton = ({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={cn('animate-pulse rounded-md bg-dark-100', className)}
+      {...props}
+    />
+  )
+}
