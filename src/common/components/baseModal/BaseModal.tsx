a879cf4fb9f29d5e89933867f84ed70efae95efa
Daria Metelitsa <dariametelitsa@gmail.com>
2024-11-13 23:58:34 +0300

    IN-196 feat: add modal to mobile menu

--- src/common/components/baseModal/BaseModal.tsx
+++ src/common/components/baseModal/BaseModal.tsx
@@ -21,7 +21,7 @@ const DialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     className={cn(
-      'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     ref={ref}
@@ -55,7 +55,7 @@ const DialogContent = React.forwardRef<
       {children}
       <DialogPrimitive.Close
         className={cn(
-          'focus-visible:outline-none focus-within:bg-dark-100 w-6 h-6 absolute lg:right-6 lg:top-4 right-3 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100  disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground',
+          'focus-visible:outline-none focus-within:bg-dark-100 w-6 h-6 absolute lg:right-6 lg:top-4 right-3 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground',
           crossOff && 'hidden'
         )}
       >
