544d19db1cbd43ed977fdb633e9610c85dd05657
KvashninAlexander2023 <141660923+KvashninAlexander2023@users.noreply.github.com>
2024-11-29 23:45:55 +0500

    IN-201 feat: Added functionality to view own posts
    * IN-201 feat: add react-intersection-observer and create folder for loader foto
    
    * [IN-201] IN-159 refactor: change structure of the project
    
    * [IN-201] IN-159 refactor: change some names and fix css
    
    * [IN-201] IN-159 refactor: change name Foto on Photo
    
    * [IN-201] IN-159 refactor: add AbortController
    
    * [IN-201] IN-159 refactor: add viewing 16 posts, heigth>965px
    
    * [IN-201] IN-159 refactor: change hight
    
    * [IN-201] IN-159 refactor: change hight on div
    
    * [IN-201] refactor: change hight inView
    
    * [IN-201] test
    
    * [IN-201] test
    
    * [IN-201] fix:  after test commit
    
    * [IN-201] add setTimeout
    
    * [IN-201] feat: load more_func if there is enough space
    
    * [IN-201] add solution test
    
    * [IN-201] feet: add cleanUp in profileStor and clearAllData
    
    * [IN-201] ref: moved profileStore to the parent directory
    
    * [IN-201] fix: fix comments
    
    ---------
    
    Co-authored-by: Daria Metelitsa <dariametelitsa@gmail.com>

--- src/common/layout/layoutForAuthorized/LayoutForAuthorized.tsx
+++ src/common/layout/layoutForAuthorized/LayoutForAuthorized.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, useEffect, useState } from 'react'
 
 import { Layout, Loader, SideBar } from '@/common'
 import { useScreenWidth } from '@/common/hooks/useScreenWidth'
-import { profileStore } from '@/features/profile'
+import { profileStore } from '@/features/profile/model/profileStore'
 import { NextPage } from 'next'
 
 export const LayoutForAuthorized: NextPage<PropsWithChildren> = ({
