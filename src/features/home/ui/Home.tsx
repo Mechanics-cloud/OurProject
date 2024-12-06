4f21400ea5c0893b3a4dc7fec0888b14d4f23fbd
Daria Metelitsa <dariametelitsa@gmail.com>
2024-12-06 17:28:46 +0300

    [IN-212] IN-230 fix: delete commit

--- src/features/home/ui/Home.tsx
+++ src/features/home/ui/Home.tsx
@@ -27,16 +27,7 @@ export const Home = observer(() => {
   }
 
   if (state?.length === 0 && !homePageStore.isLoadingHomePage) {
-    return (
-      // <div
-      //   className={
-      //     'w-[491px] h-40 border-b mt-[24px] ml-[10.9375rem] flex justify-center items-center bg-dark-100'
-      //   }
-      // >
-      //   На данный момент нет постов
-      // </div>
-      <EmptyFeed />
-    )
+    return <EmptyFeed />
   }
 
   return state?.map((item) => (
