39e01b745457f5c188dee740e27073e0d439f7d2
OchirovBair <pactuc@gmail.com>
2024-12-18 19:52:50 +0800

    IN-202 refactor: refactor public posts API types

--- src/features/publicPosts/api/publicPosts.types.ts
+++ src/features/publicPosts/api/publicPosts.types.ts
@@ -1,3 +1,5 @@
+import { BasicPost } from '@/common'
+
 export type RequestQueries = {
   endCursorPostId?: number
   pageSize?: number
@@ -13,31 +15,5 @@ export type PublicPostsDto = {
 }
 
 export type PostDto = {
-  avatarOwner: string
   avatarWhoLikes: boolean
-  createdAt: string
-  description: string
-  id: number
-  images: PostImageDto[]
-  isLiked: boolean
-  likesCount: number
-  location: string
-  owner: PostImageOwnerDto
-  ownerId: number
-  updatedAt: string
-  userName: string
-}
-
-export type PostImageDto = {
-  createdAt: string
-  fileSize: number
-  height: number
-  uploadId: string
-  url: string
-  width: number
-}
-
-export type PostImageOwnerDto = {
-  firstName: string
-  lastName: string
-}
+} & BasicPost
