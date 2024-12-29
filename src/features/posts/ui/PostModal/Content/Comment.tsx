1b050e503307ec208cddf9a983e8bc4cf88c0af2
VLAD <buladzislau@gmail.com>
2024-12-29 18:01:51 +0300

    IN-200: add post modal

--- src/features/posts/ui/PostModal/Content/Comment.tsx
+++ src/features/posts/ui/PostModal/Content/Comment.tsx
@@ -1,7 +1,7 @@
 import React, { MouseEvent, Ref, forwardRef } from 'react'
 
 import { Like, Nullable, Typography, cn } from '@/common'
-import { Avatar } from '@/common/components/avatar/Avatar'
+import { Avatar } from '@/common/components/avatar'
 import { StaticImageData } from 'next/image'
 import Link from 'next/link'
 
