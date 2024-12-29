d35982be5d66bf5156a2db309101c0f69a606222
VLAD <buladzislau@gmail.com>
2024-12-29 17:54:01 +0300

    IN-200: add like component

--- src/assets/icons/filledIcons/Heart.tsx
+++ src/assets/icons/filledIcons/Heart.tsx
@@ -1,7 +1,6 @@
 import { Ref, SVGProps, forwardRef, memo } from 'react'
 const SvgHeart = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
-    fill={'none'}
     height={'1em'}
     ref={ref}
     viewBox={'0 0 24 24'}
@@ -14,15 +13,11 @@ const SvgHeart = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
         d={
           'M12 21a1 1 0 0 1-.71-.29l-7.77-7.78a5.26 5.26 0 0 1 0-7.4 5.24 5.24 0 0 1 7.4 0L12 6.61l1.08-1.08a5.24 5.24 0 0 1 7.4 0 5.26 5.26 0 0 1 0 7.4l-7.77 7.78A1 1 0 0 1 12 21'
         }
-        fill={'currentcolor'}
       />
     </g>
     <defs>
       <clipPath id={'heart_svg__a'}>
-        <path
-          d={'M0 0h24v24H0z'}
-          fill={'#fff'}
-        />
+        <path d={'M0 0h24v24H0z'} />
       </clipPath>
     </defs>
   </svg>
