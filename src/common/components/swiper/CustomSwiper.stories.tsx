8a4e5a2fc98c3d7c751371c6d130781cbd53da7e
KvashninAlexander2023 <study_python@mail.ru>
2025-01-03 14:18:45 +0500

    [IN-224] ref: changed folder for swiper styles

--- src/common/components/swiper/CustomSwiper.stories.tsx
+++ src/common/components/swiper/CustomSwiper.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import '@/styles/customStylesForSwiper.css'
+import './customStylesForSwiper.css'
 
 import image1 from '../../../assets/images/image1.jpg'
 import CustomSwiper from './CustomSwiper'
