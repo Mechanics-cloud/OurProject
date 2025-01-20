1d67b203ea917c06b62158bb08e1d95212733ff6
OchirovBair <pactuc@gmail.com>
2025-01-20 19:11:58 +0800

    [IN-249] refactor: refactor HOCs and fix flashing auth form

--- src/common/HOC/withProtection.tsx
+++ src/common/HOC/withProtection.tsx
@@ -1,18 +1,13 @@
 import React, { useEffect } from 'react'
 
 import {
+  BaseLayoutWithStore,
   FullScreenLoader,
-  LayoutWithStore,
-  Menu,
   Paths,
-  SideBar,
-  getFromLocalStorage,
+  ProtectedLayout,
 } from '@/common'
 import { NextPageWithLayout } from '@/common/HOC/types'
-import { StorageKeys } from '@/common/enums'
-import { generalStore } from '@/core/store'
 import { authStore } from '@/features/auth'
-import { profileStore } from '@/features/profile'
 import { observer } from 'mobx-react-lite'
 import { useRouter } from 'next/router'
 
@@ -31,14 +26,10 @@ export const withProtection = <P extends object>(
   observer((props) => {
     const { isNotForAuthorizedUsers, isPublic } = options
     const router = useRouter()
-    const loading = authStore.isAuthenticated === 'pending'
     const currentAuthState = authStore.isAuthenticated
 
     useEffect(() => {
       const onRedirect = async () => {
-        if (currentAuthState === 'pending') {
-          return
-        }
         if (currentAuthState === 'yes' && isNotForAuthorizedUsers) {
           await router.replace(Paths.home)
         }
@@ -54,29 +45,21 @@ export const withProtection = <P extends object>(
       onRedirect()
     }, [isNotForAuthorizedUsers, isPublic, router, currentAuthState])
 
-    useEffect(() => {
-      if (getFromLocalStorage(StorageKeys.AccessToken)) {
-        profileStore.getProfile()
-      }
-    }, [])
-
-    if (generalStore.user) {
+    if (
+      (currentAuthState === 'yes' && isNotForAuthorizedUsers) ||
+      (currentAuthState === 'no' && !isPublic) ||
+      currentAuthState === 'pending'
+    ) {
       return (
-        <LayoutWithStore className={'flex'}>
-          <SideBar />
-          <Menu className={'lg:hidden'} />
-          <div className={'lg:pl-9 w-full lg:ml-56 pb-20'}>
-            {loading ? <FullScreenLoader /> : <PageComponent {...props} />}
-          </div>
-        </LayoutWithStore>
+        <BaseLayoutWithStore>
+          <FullScreenLoader />
+        </BaseLayoutWithStore>
       )
     }
 
     return (
-      <LayoutWithStore>
-        <div className={'lg:mx-24'}>
-          {loading ? <FullScreenLoader /> : <PageComponent {...props} />}
-        </div>
-      </LayoutWithStore>
+      <ProtectedLayout>
+        <PageComponent {...props} />
+      </ProtectedLayout>
     )
   })
--- src/common/HOC/withServerSide.tsx
+++ src/common/HOC/withServerSide.tsx
@@ -1,9 +1,6 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 
-import { LayoutWithStore, Menu, SideBar, getFromLocalStorage } from '@/common'
-import { StorageKeys } from '@/common/enums'
-import { generalStore } from '@/core/store'
-import { profileStore } from '@/features/profile'
+import { ProtectedLayout } from '@/common'
 import { observer } from 'mobx-react-lite'
 
 import { NextPageWithLayout } from './types'
@@ -12,29 +9,9 @@ export const withServerSide = <P extends object>(
   PageComponent: NextPageWithLayout<P>
 ): NextPageWithLayout<P> =>
   observer((props) => {
-    useEffect(() => {
-      if (getFromLocalStorage(StorageKeys.AccessToken)) {
-        profileStore.getProfile()
-      }
-    }, [])
-
-    if (generalStore.user) {
-      return (
-        <LayoutWithStore className={'flex'}>
-          <SideBar />
-          <Menu className={'lg:hidden'} />
-          <div className={'lg:pl-9 w-full lg:ml-56 pb-20'}>
-            <PageComponent {...props} />
-          </div>
-        </LayoutWithStore>
-      )
-    }
-
     return (
-      <LayoutWithStore>
-        <div className={'lg:mx-24'}>
-          <PageComponent {...props} />
-        </div>
-      </LayoutWithStore>
+      <ProtectedLayout>
+        <PageComponent {...props} />
+      </ProtectedLayout>
     )
   })
