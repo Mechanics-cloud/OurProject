a46f2c1ba61bf919aed092ed8c7e1c9e39f980d2
OchirovBair <pactuc@gmail.com>
2025-01-24 12:44:13 +0800

    [IN-259] refactor: refactor

--- src/pages/github/index.tsx
+++ src/pages/github/index.tsx
@@ -1,28 +1,77 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import * as React from 'react'
+import { toast } from 'react-toastify'
 
-import { FullScreenLoader, Paths, withProtection } from '@/common'
+import notFound from '@/assets/images/notFound.svg'
+import {
+  BaseLayoutWithStore,
+  Button,
+  FullScreenLoader,
+  Paths,
+  Typography,
+  responseErrorHandler,
+  useTranslation,
+  withProtection,
+} from '@/common'
 import { StorageKeys } from '@/common/enums'
 import { setToLocalStorage } from '@/common/utils/localStorage'
 import { authStore } from '@/features/auth'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 const GitHubCallback = () => {
   const router = useRouter()
+  const { t } = useTranslation()
+  const [isLoginViaGithub, setIsLoginViaGithub] = useState<boolean>(true)
+  const onSignInHandler = () => {
+    router.push(`${Paths.signIn}`)
+  }
 
   useEffect(() => {
-    if (router.query.accessToken) {
-      const { accessToken } = router.query
+    debugger
+    const { accessToken } = router.query
 
+    if (router.isReady) {
       if (accessToken) {
         setToLocalStorage(StorageKeys.AccessToken, accessToken as string)
         authStore.me().then(() => router.push(`${Paths.home}`))
       } else {
-        throw new Error('no token')
+        setIsLoginViaGithub(false)
+        responseErrorHandler(new Error('Авторизация через GitHub невозможна'))
       }
     }
   }, [router])
 
-  return <FullScreenLoader />
+  return isLoginViaGithub ? (
+    <BaseLayoutWithStore>
+      <FullScreenLoader />
+    </BaseLayoutWithStore>
+  ) : (
+    <BaseLayoutWithStore
+      className={'flex justify-center items-center w-full h-headCalc'}
+    >
+      <div
+        className={
+          'mt-20 lg:mt-40 flex flex-col gap-8 items-center justify-center'
+        }
+      >
+        <Image
+          alt={'404 image'}
+          className={'opacity-90'}
+          height={500}
+          src={notFound}
+          width={500}
+        />
+        <Typography
+          className={'m-auto text-center font-normal'}
+          variant={'h1'}
+        >
+          {t.basic.notFoundTitle}
+        </Typography>
+        <Button onClick={onSignInHandler}>{t.basic.pagination.goBack}</Button>
+      </div>
+    </BaseLayoutWithStore>
+  )
 }
 
-export default withProtection(GitHubCallback)
+export default GitHubCallback
