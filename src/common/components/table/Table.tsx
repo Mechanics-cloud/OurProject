df5b12cd178773388f19645ecb53ab28ec714bbe
Sun-ny11 <spetrow79@gmail.com>
2025-02-07 12:09:23 +0300

    [IN-279] init: create base Table component

---
+++ src/common/components/table/Table.tsx
@@ -0,0 +1,66 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import { cn } from '@/common/utils'
+
+const Root = ({
+  className,
+  ...restProps
+}: ComponentPropsWithoutRef<'table'>) => {
+  return (
+    <table
+      className={cn('border-spacing-[0] border-collapse w-full ', className)}
+      {...restProps}
+    />
+  )
+}
+const Head = ({
+  className,
+  ...restProps
+}: ComponentPropsWithoutRef<'thead'>) => {
+  return (
+    <thead
+      className={cn('bg-dark-500', className)}
+      {...restProps}
+    />
+  )
+}
+const Body = ({
+  className,
+  ...restProps
+}: ComponentPropsWithoutRef<'tbody'>) => {
+  return (
+    <tbody
+      className={cn(className)}
+      {...restProps}
+    />
+  )
+}
+const Row = ({ className, ...restProps }: ComponentPropsWithoutRef<'tr'>) => {
+  return (
+    <tr
+      className={cn('border border-dark-500', className)}
+      {...restProps}
+    />
+  )
+}
+const HeaderCell = ({
+  className,
+  ...restProps
+}: ComponentPropsWithoutRef<'th'>) => {
+  return (
+    <th
+      className={cn('text-left', className)}
+      {...restProps}
+    />
+  )
+}
+const Cell = ({ className, ...restProps }: ComponentPropsWithoutRef<'td'>) => {
+  return (
+    <td
+      className={cn(className)}
+      {...restProps}
+    />
+  )
+}
+
+export const Table = { Body, Cell, Head, HeaderCell, Root, Row }
