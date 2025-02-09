e67ed0140504e7d626f753e1bc1ba819d3266c1b
KvashninAlexander2023 <study_python@mail.ru>
2025-02-09 22:40:04 +0500

    [IN-276] feat: add  confirm the transition

--- src/features/profile/settings/management/api/profile.management.api.ts
+++ src/features/profile/settings/management/api/profile.management.api.ts
@@ -1,12 +1,12 @@
 import { instance } from '@/common/api'
 import { AxiosInstance, AxiosResponse } from 'axios'
 
-import { dataProps } from './management.types.api'
+import { DataSubscriptionApi } from './management.types.api'
 
 class SubscriptionsApi {
   constructor(private instance: AxiosInstance) {}
 
-  public subscriptions(data: dataProps): Promise<AxiosResponse> {
+  public subscriptions(data: DataSubscriptionApi): Promise<AxiosResponse> {
     return this.instance.post(`/v1/subscriptions`, data)
   }
 }
--- src/features/profile/settings/management/ui/management.tsx
+++ src/features/profile/settings/management/ui/management.tsx
@@ -1,45 +1,37 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 
 import { PaypalSvgrepoCom4, StripeSvgrepoCom4 } from '@/assets/icons'
-import { useTranslation } from '@/common'
+import { ConfirmModal, Loader, useTranslation } from '@/common'
 import { useRouter } from 'next/router'
 
+import { DataSubscription } from '../api/management.types.api'
 import { subscriptionsApi } from '../api/profile.management.api'
 
 export const Management = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isLoad, setIsLoad] = useState(false)
+  const [selectedBank, setSelectedBank] = useState<'paypal' | 'stripe' | null>(
+    null
+  )
   const { t } = useTranslation()
   const router = useRouter()
 
+  // data: DataSubscription - передать при вызове оплаты
   // https://docs.stripe.com/testing#cards-responses
 
   const { success } = router.query
 
-  useEffect(() => {
-    if (success === 'true') {
-      toast.success('Оплата прошла успешно!')
-    } else if (success === 'false') {
-      toast.error('Оплата не прошла !')
-    }
-  }, [success])
-
   // STRIPE
-  const dataSTRIPE = {
+  const data: DataSubscription = {
     amount: 10,
     baseUrl: 'http://localhost:3000/profile/settings/management',
-    paymentType: 'STRIPE',
-    typeSubscription: 'DAY',
-  } as const
-
-  // PAYPAL
-  const dataPAYPAL = {
-    amount: 10,
-    baseUrl: 'http://localhost:3000/profile/settings/management',
-    paymentType: 'PAYPAL',
     typeSubscription: 'DAY',
   } as const
 
   const onStripe = async () => {
+    const dataSTRIPE = { ...data, paymentType: 'STRIPE' as const }
+
     try {
       const response = await subscriptionsApi.subscriptions(dataSTRIPE)
       const redirectUrl = response.data?.url
@@ -54,6 +46,8 @@ export const Management = () => {
     }
   }
   const onPayPal = async () => {
+    const dataPAYPAL = { ...data, paymentType: 'PAYPAL' as const }
+
     try {
       const response = await subscriptionsApi.subscriptions(dataPAYPAL)
       const redirectUrl = response.data?.url
@@ -68,17 +62,83 @@ export const Management = () => {
     }
   }
 
+  const onConfirm = () => {
+    setIsModalOpen(false)
+    if (selectedBank === 'paypal') {
+      onPayPal()
+      setIsLoad(true)
+    } else if (selectedBank === 'stripe') {
+      onStripe()
+      setIsLoad(true)
+    }
+  }
+
+  const onCancel = () => {
+    setIsModalOpen(false)
+    setSelectedBank(null)
+  }
+
+  useEffect(() => {
+    return setIsLoad(false)
+  }, [])
+
+  useEffect(() => {
+    if (success === 'true') {
+      toast.success('Оплата прошла успешно!')
+      const { success: _, ...restQuery } = router.query
+
+      router.replace(
+        { pathname: router.pathname, query: restQuery },
+        undefined,
+        { shallow: true }
+      )
+    } else if (success === 'false') {
+      toast.error('Оплата не прошла!')
+      const { success: _, ...restQuery } = router.query
+
+      router.replace(
+        { pathname: router.pathname, query: restQuery },
+        undefined,
+        { shallow: true }
+      )
+    }
+  }, [success, router])
+
+  if (isLoad) {
+    return <Loader />
+  }
+
   return (
     <div className={'mt-8 w-full flex justify-end items-center'}>
-      <PaypalSvgrepoCom4
-        className={'w-36 h-[68px] m-2 cursor-pointer'}
-        onClick={onPayPal}
-      />
-      <span>OR</span>
-      <StripeSvgrepoCom4
-        className={'w-36 h-[68px] m-2 cursor-pointer'}
-        onClick={onStripe}
-      />
+      <button
+        className={'w-[96px] h-[60px]'}
+        onClick={() => {
+          setSelectedBank('paypal')
+          setIsModalOpen(true)
+        }}
+        type={'button'}
+      >
+        <PaypalSvgrepoCom4 className={'w-full h-full'} />
+      </button>
+      <span className={'m-2'}>OR</span>
+      <button
+        className={'w-[96px] h-[60px]'}
+        onClick={() => {
+          setSelectedBank('stripe')
+          setIsModalOpen(true)
+        }}
+        type={'button'}
+      >
+        <StripeSvgrepoCom4 className={'w-full h-full'} />
+      </button>
+      <ConfirmModal
+        onClick={onConfirm}
+        onClose={onCancel}
+        open={isModalOpen}
+        title={'Подтверждение перехода'}
+      >
+        Вы будете перенаправлены на сайт банка для оплаты. Продолжить?
+      </ConfirmModal>
     </div>
   )
 }
