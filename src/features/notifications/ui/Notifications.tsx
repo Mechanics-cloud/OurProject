64fc0a7fbdc6e7803e0ea1e094644869503ed792
tanya-kostecki <tanya.pavlova1294@gmail.com>
2025-03-06 18:46:23 +0100

    IN-286 feat: add animation when notification is deleted

--- src/features/notifications/ui/Notifications.tsx
+++ src/features/notifications/ui/Notifications.tsx
@@ -1,49 +1,23 @@
-import { Close } from '@/assets/icons'
-import { ScrollArea, Typography, timeAgo } from '@/common'
-import { useNotifications } from '@/features/notifications'
+import { ScrollArea, Typography, useTranslation } from '@/common'
+import { notificationsStore } from '@/features/notifications'
+import { NotificationsItem } from '@/features/notifications/ui/NotificationsItem'
+import { AnimatePresence } from 'framer-motion'
 import { observer } from 'mobx-react-lite'
 
 export const Notifications = observer(() => {
-  const { deleteNotification, elementsRef, notifications, router, t } =
-    useNotifications()
+  const { notifications } = notificationsStore
+  const { t } = useTranslation()
 
   return notifications?.length && notifications.length > 0 ? (
     <ScrollArea className={'h-[calc(100%-60px)]'}>
-      {notifications?.map((item) => (
-        <div
-          className={'py-3 border-t-[1px] border-t-dark-100 text-left'}
-          id={String(item.id)}
-          key={item.id}
-          ref={(elem) => {
-            elem && elementsRef.current.set(item.id, elem)
-          }}
-        >
-          <div className={'flex justify-between'}>
-            {!item.isRead && (
-              <Typography variant={'bold14'}>
-                {t.notification.item}{' '}
-                <b className={'text-accent-900'}>{t.notification.new}</b>
-              </Typography>
-            )}
-            <Close
-              className={'ml-auto'}
-              onClick={() => deleteNotification(item.id)}
-            />
-          </div>
-          <Typography
-            className={'mt-2'}
-            variant={'reg14'}
-          >
-            {item.message}
-          </Typography>
-          <Typography
-            className={'text-light-900'}
-            variant={'small'}
-          >
-            {timeAgo(item.createdAt, router.locale)}
-          </Typography>
-        </div>
-      ))}
+      <AnimatePresence>
+        {notifications?.map((item) => (
+          <NotificationsItem
+            item={item}
+            key={item.id}
+          />
+        ))}
+      </AnimatePresence>
     </ScrollArea>
   ) : (
     <Typography
