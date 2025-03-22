9ca0259bc64cee38beb18fb038e982967bfa74ae
KvashninAlexander2023 <study_python@mail.ru>
2025-03-22 23:20:00 +0500

    [IN-298] chore: add styles

--- src/features/profile/ui/Profile.tsx
+++ src/features/profile/ui/Profile.tsx
@@ -39,7 +39,7 @@ export const Profile = observer(({ screenSize, store }: Props) => {
   const isAuthenticated = !!profileStore.userProfile
   const isOwnProfile = profileStore.userProfile?.id === store.userProfile?.id
 
-  const { isMobile } = useScreenWidth(screenSize)
+  const { isMobile, isTablet } = useScreenWidth(screenSize)
   const { isPaid } = usePaidAccount()
 
   useEffect(() => {
@@ -48,81 +48,84 @@ export const Profile = observer(({ screenSize, store }: Props) => {
     }
   }, [isAuthenticated])
 
-  //TODO проверить типы, попробовать сделать гридами
+  //TODO проверить типы
 
   return (
     <UserIdProvider ctx={query.id ? +query.id[0] : null}>
-      <div className={'flex w-full'}>
-        <div className={'flex flex-col w-full'}>
+      <div className={'flex w-full flex-col'}>
+        <div
+          className={
+            'grid grid-cols-[auto_1fr] w-full gap-x-2 gap-y-1 mt-4 mb-3 md:mt-9 sm-500:gap-x-5 lg:mb-[53px] md:gap-x-9 md:grid-cols-[auto_1fr_auto]'
+          }
+        >
+          <Image
+            alt={'avatar'}
+            className={
+              'rounded-full row-span-2 md:row-start-1 md:col-start-1 md:row-span-3'
+            }
+            height={isMobile ? 100 : 200}
+            priority
+            src={avatar || avatarPlaceholder}
+            width={isMobile ? 100 : 200}
+          />
+          <div
+            className={`gap-3 row-start-3 col-span-2 mt-0.5 sm-500:col-span-1 md:row-start-1 md:col-start-2 md:mb-0 md:content-center ${
+              isOwnProfile ? 'mb-0' : 'mb-3.5'
+            }`}
+          >
+            <Typography
+              className={
+                ' text-light-100 flex gap-2 items-center sm-500:justify-center md:justify-start whitespace-nowrap'
+              }
+              variant={'h1'}
+            >
+              {store.userProfile?.userName ?? 'URL Profile'}
+              {isPaid && <Paid />}
+            </Typography>
+          </div>
+
           <div
             className={
-              'mt-5 md:mt-9 flex flex-col items-center w-full gap-5 mb-3 lg:gap-9 lg:mb-[53px] xs:flex-row'
+              'row-start-4 col-span-2 flex justify-center flex-col md:col-start-3 md:row-start-1 md:col-span-1'
             }
           >
-            <Image
-              alt={'avatar'}
-              className={'rounded-full pr-0'}
-              height={isMobile ? 100 : 200}
-              priority
-              src={avatar || avatarPlaceholder}
-              width={isMobile ? 100 : 200}
-            />
-            <div className={'flex flex-col flex-wrap w-full'}>
-              <div
-                className={
-                  'hidden md:flex items-center justify-between w-full mb-5'
-                }
-              >
-                <Typography
-                  className={'flex text-light-100 items-center gap-3'}
-                  variant={'h1'}
+            {isOwnProfile ? (
+              !isTablet && (
+                <Button
+                  className={'flex'}
+                  variant={'secondary'}
                 >
-                  {store.userProfile?.userName ?? 'URL Profile'}
-                  {isPaid && <Paid />}
-                </Typography>
-                {isOwnProfile ? (
-                  <Button
-                    className={'hidden md:block'}
-                    variant={'secondary'}
-                  >
-                    <Link href={ProtectedPaths.profileSettings}>
-                      {settingsButton}
-                    </Link>
-                  </Button>
-                ) : (
-                  <FollowButtonsContainer userId={store.userProfile?.id} />
-                )}
+                  <Link href={ProtectedPaths.profileSettings}>
+                    {settingsButton}
+                  </Link>
+                </Button>
+              )
+            ) : (
+              <div className={'flex gap-3 flex-col flex-wrap md:flex-row'}>
+                <FollowButtonsContainer userId={store.userProfile?.id} />
+                <Button variant={'secondary'}>Send Message</Button>
               </div>
-              <ProfileStatistics
-                followers={followers}
-                following={following}
-                isMobile={isMobile}
-                publications={publications}
-                userMetadata={store.userProfile.userMetadata}
-              />
-              <ProfileAboutMe
-                aboutMe={store.userProfile?.aboutMe}
-                className={'hidden lg:block lg:mt-6'}
-                isMobile={isMobile}
-              />
-            </div>
+            )}
           </div>
-          <Typography
-            className={'md:hidden flex text-light-100 items-center gap-3 mb-3'}
-            variant={'h1'}
-          >
-            {store.userProfile?.userName ?? 'URL Profile'}
-            <Paid />
-          </Typography>
-          <FollowButtonsContainer
-            className={'md:hidden flex-1'}
-            userId={store.userProfile?.id}
+          <ProfileStatistics
+            className={
+              'row-start-1 col-start-2 row-span-2 self-center md:row-start-2 md:col-start-2 md:row-span-1 md:col-span-2'
+            }
+            followers={followers}
+            following={following}
+            isMobile={isMobile}
+            publications={publications}
+            userMetadata={store.userProfile.userMetadata}
           />
           <ProfileAboutMe
             aboutMe={store.userProfile?.aboutMe}
-            className={'lg:hidden block mt-7'}
+            className={`lg:mt-6 row-start-5 col-span-2 md:col-start-2 md:row-start-3 ${
+              isOwnProfile ? 'mb-2 mt-0' : 'mt-3 mb-4'
+            }`}
             isMobile={isMobile}
           />
+        </div>
+        <div className={'flex flex-col w-full  md:mt-6'}>
           <PhotoProfilePostsGallery store={store} />
         </div>
       </div>
